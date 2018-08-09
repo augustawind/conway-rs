42b7c3804b7590083c602d63b12cce5c9cde1aaa
Dustin Rohde <dustin.rohde@gmail.com>
2018-08-09 11:08:23 -0400

    get ducks in a row

--- conway-app/src/http.rs
+++ conway-app/src/http.rs
@@ -4,19 +4,25 @@ use rocket;
 use rocket::response::NamedFile;
 
 lazy_static! {
-    static ref BASE_DIR: &'static Path = Path::new("client/dist/");
+    static ref DIST_DIR: &'static Path = Path::new("client/dist/");
+    static ref VENDOR_DIR: &'static Path = Path::new("client/vendor/");
 }
 
 pub fn server() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![route_index, route_static])
+    rocket::ignite().mount("/", routes![route_index, route_static, route_vendor])
 }
 
 #[get("/", format = "text/html")]
 fn route_index() -> Option<NamedFile> {
-    NamedFile::open(BASE_DIR.join("index.html")).ok()
+    NamedFile::open(DIST_DIR.join("index.html")).ok()
 }
 
 #[get("/static/<file..>")]
 fn route_static(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(BASE_DIR.join(file)).ok()
+    NamedFile::open(DIST_DIR.join(file)).ok()
+}
+
+#[get("/vendor/<file..>")]
+fn route_vendor(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(VENDOR_DIR.join(file)).ok()
 }
