d80efb103eb58ced04b722044253ea652a82e582
Dustin Rohde <dustin.rohde@gmail.com>
2018-08-09 07:40:09 -0400

    clean: client-side
    mv static/ -> client/
    add package.json

--- conway-app/src/http.rs
+++ conway-app/src/http.rs
@@ -4,17 +4,7 @@ use rocket;
 use rocket::response::NamedFile;
 
 lazy_static! {
-    static ref PATH_STATIC: &'static Path = Path::new("static/");
-    static ref CONTEXT: &'static Context = &Context {
-        title: "Conway's Game of Life",
-        default_grid: include_str!("../static/patterns/default"),
-    };
-}
-
-#[derive(Serialize)]
-struct Context {
-    title: &'static str,
-    default_grid: &'static str,
+    static ref BASE_DIR: &'static Path = Path::new("client/");
 }
 
 pub fn server() -> rocket::Rocket {
@@ -23,10 +13,10 @@ pub fn server() -> rocket::Rocket {
 
 #[get("/", format = "text/html")]
 fn route_index() -> Option<NamedFile> {
-    NamedFile::open(PATH_STATIC.join("index.html")).ok()
+    NamedFile::open(BASE_DIR.join("dist/index.html")).ok()
 }
 
 #[get("/static/<file..>")]
 fn route_static(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(PATH_STATIC.join(file)).ok()
+    NamedFile::open(BASE_DIR.join(file)).ok()
 }
